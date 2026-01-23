4e2a2ff6ab0b946dcb4367cb77965c682c2afef7
Tubha Ahmad <toubha.ah@hotmail.com>
2026-01-23 19:45:08 +0100

    Build all HTML pages with Vite

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        register: resolve(__dirname, "register.html"),
+        login: resolve(__dirname, "login.html"),
+        create: resolve(__dirname, "create.html"),
+      },
+    },
+  },
+});
